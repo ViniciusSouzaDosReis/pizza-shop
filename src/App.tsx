e10242c672c2730a230e0f41eb4ff8ac3ead841a
Vinicius Reis <vinireis010305@gmail.com>
2024-03-04 17:11:28 -0300

    Páginas e componentes (UI) - ThemeToggle e menu da conta

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
+import { ThemeProvider } from './components/themes/theme-provider'
 import { router } from './routes'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Toaster richColors />
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
+      <ThemeProvider defaultTheme="dark" storageKey="pizza.shop">
+        <Toaster richColors />
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
