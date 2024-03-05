bef7e8e5e7ae097ecb50a3708629485a70504160
Vinicius Reis <vinireis010305@gmail.com>
2024-03-05 19:59:54 -0300

    Conexão com API - Atualizando perfil

--- src/components/store-profile-dialog.tsx
+++ src/components/store-profile-dialog.tsx
@@ -1,11 +1,14 @@
-import { useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery } from '@tanstack/react-query'
 import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
 import { z } from 'zod'
 
 import { getMenagedRestaurant } from '@/api/get-menaged-restaurant'
+import { updateProfile } from '@/api/update-profile'
 
 import { Button } from './ui/button'
 import {
+  DialogClose,
   DialogContent,
   DialogDescription,
   DialogFooter,
@@ -27,14 +30,28 @@ export function StoreProfileDialog() {
   const { data: restaurant } = useQuery({
     queryKey: ['restaurant'],
     queryFn: getMenagedRestaurant,
+    staleTime: Infinity,
   })
 
-  const { register } = useForm<EditStoreProfileForm>({
+  const { mutateAsync: updateProfileFn } = useMutation({
+    mutationFn: updateProfile,
+  })
+
+  const { register, handleSubmit } = useForm<EditStoreProfileForm>({
     values: {
       name: restaurant?.name ?? '',
       description: restaurant?.description ?? '',
     },
   })
+
+  async function handleUpdateProfile(data: EditStoreProfileForm) {
+    try {
+      await updateProfileFn({ name: data.name, description: data.description })
+      toast.success('Informações atualizadas com sucesso.')
+    } catch {
+      toast.error('Erro ao tentar atualizar as informações.')
+    }
+  }
 
   return (
     <DialogContent>
@@ -45,7 +62,7 @@ export function StoreProfileDialog() {
         </DialogDescription>
       </DialogHeader>
 
-      <form>
+      <form onSubmit={handleSubmit(handleUpdateProfile)}>
         <div className="space-y-4 py-4">
           <div className="grid grid-cols-4 items-center gap-4">
             <Label className="text-right" htmlFor="name">
@@ -67,9 +84,11 @@ export function StoreProfileDialog() {
         </div>
 
         <DialogFooter>
-          <Button variant="ghost" type="button">
-            Cancelar
-          </Button>
+          <DialogClose asChild>
+            <Button variant="ghost" type="button">
+              Cancelar
+            </Button>
+          </DialogClose>
           <Button type="submit" variant="success">
             Salvar
           </Button>
